fdddbfc1cf428059414db489390ae0c4ac915315
SilentJs <86762707+SilentJs@users.noreply.github.com>
2022-04-29 04:58:31 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000');
+const socket = io('https://exquisite-custard-45c4ef.netlify.app/');
 const messages = document.getElementById('messages');
 const msgForm = document.getElementById('msgForm');
 
